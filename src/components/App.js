7d649f041af2692e81818eedc0c003df93e9550c
Andrew Cook <andrewjcook93@gmail.com>
2022-10-28 17:00:31 -0500

    see details ternary functional (my post or not my post)

--- src/components/App.js
+++ src/components/App.js
@@ -1,71 +1,87 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./Navbar";
-import {Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 // import {router} from "../index.js"
 
 const App = () => {
-    const [ourProducts, setOurProducts] = useState([]);
-    const [currentProfile, setCurrentProfile] = useState({});
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    // const [myPosts, setMyPosts] = useState([]);
-    //useEffect, if there is a token in local storage, then run GET route /users/me
-    useEffect (()=> {
-        if (localStorage.getItem('token')) {
-            async function fetchUserData () {
-                try {
-                    const response = await fetch("https://strangers-things.herokuapp.com/api/2209-ftb-mt-web-ft/users/me", 
-                    {
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'Authorization': `Bearer ${localStorage.getItem('token')}`
-                        },
-                    })
-                    const userData = await response.json();
-                    setCurrentProfile(userData.data)
-                    // console.log(currentProfile)
-                    // console.log(userData)
-                } catch (error) {
-                    console.log(error)
-                }
+  const [ourProducts, setOurProducts] = useState([]);
+  const [currentProfile, setCurrentProfile] = useState({});
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [myPosts, setMyPosts] = useState([]);
+  //useEffect, if there is a token in local storage, then run GET route /users/me
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      async function fetchUserData() {
+        try {
+          const response = await fetch(
+            "https://strangers-things.herokuapp.com/api/2209-ftb-mt-web-ft/users/me",
+            {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+              },
             }
-            fetchUserData();
+          );
+          const userData = await response.json();
+          setCurrentProfile(userData.data);
+          // console.log(currentProfile)
+          // console.log(userData)
+        } catch (error) {
+          console.log(error);
         }
-        async function fetchProductData () {
-            try {
-                const response = await fetch("https://strangers-things.herokuapp.com/api/2209-ftb-mt-web-ft/posts");
-                
-                const productData = await response.json(); 
-                // console.log('I am the response ', productData)
-                setOurProducts(productData.data.posts);
-                // console.log(productData.data.posts)
-            } catch (err) {
-                console.log(err);
-            }
-        }
-        fetchProductData();
-    },[])
-    // console.log(currentProfile);
-    useEffect(()=> { 
-        if (currentProfile._id){
-            setIsLoggedIn(true)
-        } 
-    }, [currentProfile]);
-    // console.log(isLoggedIn);
+      }
+      fetchUserData();
+    }
+    async function fetchProductData() {
+      try {
+        const response = await fetch(
+          "https://strangers-things.herokuapp.com/api/2209-ftb-mt-web-ft/posts"
+        );
 
-    return (
-        <div> 
-            <header>
-                <h1><a href="https://drewfordslist.netlify.app">Drewford's List</a></h1>
-            </header>
-            <Navbar isLoggedIn={isLoggedIn} className="nav" 
-            // isHome = {isHome} setIsHome = {setIsHome} 
-            />
-            <Outlet context={{productObj: [ourProducts, setOurProducts], profileObj: [currentProfile, setCurrentProfile], loggedInObj: [isLoggedIn, setIsLoggedIn]}}/>
-            <footer>
-                <p>Developed by <span id="author">Drewford</span></p>
-            </footer>
-        </div>
-    )
-}
+        const productData = await response.json();
+        // console.log('I am the response ', productData)
+        setOurProducts(productData.data.posts);
+        // console.log(productData.data.posts)
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchProductData();
+  }, []);
+  // console.log(currentProfile);
+  useEffect(() => {
+    if (currentProfile._id) {
+      setIsLoggedIn(true);
+    }
+  }, [currentProfile]);
+  // console.log(isLoggedIn);
+
+  return (
+    <div>
+      <header>
+        <h1>
+          <a href="https://drewfordslist.netlify.app">Drewford's List</a>
+        </h1>
+      </header>
+      <Navbar
+        isLoggedIn={isLoggedIn}
+        className="nav"
+        // isHome = {isHome} setIsHome = {setIsHome}
+      />
+      <Outlet
+        context={{
+          productObj: [ourProducts, setOurProducts],
+          profileObj: [currentProfile, setCurrentProfile],
+          loggedInObj: [isLoggedIn, setIsLoggedIn],
+        }}
+      />
+      <footer>
+        <p>
+          Developed by <span id="author">Drewford</span>
+        </p>
+      </footer>
+    </div>
+  );
+};
 
 export default App;
--- src/components/Details.js
+++ src/components/Details.js
@@ -2,71 +2,97 @@ import { useOutletContext, useParams } from "react-router-dom";
 import SendMessage from "./SendMessage";
 import EditPost from "./EditPostForm";
 import DeletePost from "./DeletePost";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const Details = () => {
-  const {productObj: [ourProducts, setOurProducts]} = useOutletContext();
+  const {
+    productObj: [ourProducts, setOurProducts],
+  } = useOutletContext();
+  const {
+    profileObj: [currentProfile, setCurrentProfile],
+  } = useOutletContext();
   const [toggleEdit, setToggleEdit] = useState(false);
   const [toggleSendMessage, setToggleSendMessage] = useState(false);
-  const {id} = useParams();
+  const { id } = useParams();
   const reverseProducts = [...ourProducts].reverse();
   const [product, setProduct] = useState({});
-  // const [currentProfile, setCurrentProfile] = useOutletContext({});
 
-  useEffect (() => {
+  useEffect(() => {
     const filteredProducts = ourProducts.filter((singleProduct) => {
       return id === singleProduct._id;
-    }) 
+    });
     // console.log(filteredProducts)
     setProduct(filteredProducts[0]);
-  }, [])
-  
-  function changeToggleEdit () {
+  }, []);
+
+  function changeToggleEdit() {
     toggleEdit ? setToggleEdit(false) : setToggleEdit(true);
   }
 
-  function changeToggleSendMessage () {
-    toggleSendMessage ? setToggleSendMessage(false) : setToggleSendMessage(true);
+  function changeToggleSendMessage() {
+    toggleSendMessage
+      ? setToggleSendMessage(false)
+      : setToggleSendMessage(true);
   }
 
   if (product._id) {
-    
-  
     return (
       <div className="product">
         {/* Post Details */}
-        <p><span className="name-detail"><b>{product.title}</b></span></p>
-        <p><span className="price-detail">{product.price}</span></p>
-        <p>Seller: <span className="seller-detail"></span>{product.author.username}</p>
-        <p>Description: <span className="description-detail">{product.description}</span></p>
+        <p>
+          <span className="name-detail">
+            <b>{product.title}</b>
+          </span>
+        </p>
+        <p>
+          <span className="price-detail">{product.price}</span>
+        </p>
+        <p>
+          Seller: <span className="seller-detail"></span>
+          {product.author.username}
+        </p>
+        <p>
+          Description:{" "}
+          <span className="description-detail">{product.description}</span>
+        </p>
         {/* Contact User or Edit This Post */}
 
-        {/* attempt at ternary. 
+        {/* attempt at ternary.  */}
         {console.log(product)}
-        {{product.author.username} == {currentProfile} ? 
-        return <div>
-          <p>my post</p>
-        </div> : <p>not my post</p>
-        } */}
+        {product.author.username == currentProfile.username ? (
+          <div>
+            <button onClick={changeToggleEdit} className="details-bttn">
+              Edit
+            </button>
+          </div>
+        ) : (
+          <div>
+            <button
+              onClick={changeToggleSendMessage}
+              type="submit"
+              className="details-bttn"
+            >
+              Contact Seller
+            </button>
+          </div>
+        )}
 
-        <button onClick={changeToggleSendMessage} type="submit" className="details-bttn">Contact Seller</button>
         {/* toggle send message content */}
-        { toggleSendMessage ? <SendMessage product = {product} /> : null}
-        <br /><br />
-        <button onClick={changeToggleEdit} className="details-bttn">Edit</button>
+        {toggleSendMessage ? <SendMessage product={product} /> : null}
+
         {/*pass down product to EditPost */}
-        { toggleEdit ? < EditPost product = {product} /> : null}
+        {toggleEdit ? <EditPost product={product} /> : null}
         {/* <button>Delete</button> */}
-        { toggleEdit ? <DeletePost product = {product} /> : null}
+        {toggleEdit ? <DeletePost product={product} /> : null}
       </div>
-    )
+    );
   } else {
     return (
       <div>
         <p>Post inactive</p>
       </div>
-    )
+    );
   }
-}
+};
 
 export default Details;
